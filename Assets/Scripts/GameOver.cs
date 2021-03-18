7673f4422a62d0121a2af27028367d5946992bcb
Deepak Agrawal <debug.career@gmail.com>
2021-03-18 14:15:54 +0530

    Code formatted

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -5,10 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-
     public void Setup(bool val)
     {
-        gameObject.SetActive(val);
+        gameObject.SetActive (val);
     }
 
     public void RestartButton()
@@ -20,5 +19,4 @@ public class GameOver : MonoBehaviour
     {
         SceneManager.LoadScene("level1");
     }
-
 }
